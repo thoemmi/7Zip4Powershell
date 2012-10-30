3a33dee3c456dae1e1c59e30435c91f82ce5b3dd
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-30 13:37:57 +0100

    run 7-Zip in background thread, so we can terminate it

--- 7Zip4Powershell/Expand7Zip.cs
+++ 7Zip4Powershell/Expand7Zip.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Management.Automation;
+using System.Threading;
 using SevenZip;
 
 namespace SevenZip4Powershell {
@@ -13,25 +15,53 @@ namespace SevenZip4Powershell {
         [ValidateNotNullOrEmpty]
         public string Directory { get; set; }
 
+        private readonly BlockingCollection<object> _writings = new BlockingCollection<object>();
+        private Thread _thread;
+
         protected override void EndProcessing() {
-            var activity = String.Format("Extracting {0} to {1}", System.IO.Path.GetFileName(FileName), Directory);
-            var statusDescription = "Extracting";
-
-            WriteObject(String.Format("Extracting archive {0}", FileName));
-            WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = 0 });
-
-            using (var extractor = new SevenZipExtractor(FileName)) {
-                extractor.Extracting += (sender, args) => 
-                    WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = args.PercentDone });
-                extractor.FileExtractionStarted += (sender, args) => {
-                    statusDescription = String.Format("Extracting file {0}", args.FileInfo.FileName);
-                    WriteObject(statusDescription);
-                };
-                extractor.ExtractArchive(Directory);
+            _thread = new Thread(() => Expand(FileName, Directory, _writings)) { IsBackground = true };
+            _thread.Start();
+            foreach (var obj in _writings.GetConsumingEnumerable()) {
+                var progress = obj as ProgressRecord;
+                if (progress != null) {
+                    WriteProgress(progress);
+                } else {
+                    WriteObject(obj);
+                }
             }
+            _thread.Join();
+        }
 
-            WriteProgress(new ProgressRecord(0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
-            WriteObject("Extraction finished");
+        protected override void StopProcessing() {
+            if (_thread != null) {
+                _thread.Abort();
+            }
+        }
+
+        private static void Expand(string filename, string directory, BlockingCollection<object> writings) {
+            try {
+                var activity = String.Format("Extracting {0} to {1}", System.IO.Path.GetFileName(filename), directory);
+                var statusDescription = "Extracting";
+
+                writings.Add(String.Format("Extracting archive {0}", filename));
+                writings.Add(new ProgressRecord(0, activity, statusDescription) { PercentComplete = 0 });
+
+                using (var extractor = new SevenZipExtractor(filename)) {
+                    extractor.Extracting += (sender, args) =>
+                                            writings.Add(new ProgressRecord(0, activity, statusDescription)
+                                            {PercentComplete = args.PercentDone});
+                    extractor.FileExtractionStarted += (sender, args) => {
+                        statusDescription = String.Format("Extracting file {0}", args.FileInfo.FileName);
+                        writings.Add(statusDescription);
+                    };
+                    extractor.ExtractArchive(directory);
+                }
+
+                writings.Add(new ProgressRecord(0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
+                writings.Add("Extraction finished");
+            } finally {
+                writings.CompleteAdding();
+            }
         }
     }
 }
