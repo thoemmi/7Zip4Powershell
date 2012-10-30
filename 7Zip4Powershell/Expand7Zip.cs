fa3bd6a2a4e7d544a546e56bcf61fc3c9321142c
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-30 11:39:10 +0100

    more logging

--- 7Zip4Powershell/Expand7Zip.cs
+++ 7Zip4Powershell/Expand7Zip.cs
@@ -14,21 +14,24 @@ namespace SevenZip4Powershell {
         public string Directory { get; set; }
 
         protected override void EndProcessing() {
-            var activity = String.Format("Extracting {0} to {1}", FileName, Directory);
+            var activity = String.Format("Extracting {0} to {1}", System.IO.Path.GetFileName(FileName), Directory);
             var statusDescription = "Extracting";
 
+            WriteObject(String.Format("Extracting archive {0}", FileName));
+            WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = 0 });
+
             using (var extractor = new SevenZipExtractor(FileName)) {
-                extractor.Extracting += (sender, args) =>
+                extractor.Extracting += (sender, args) => 
                     WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = args.PercentDone });
                 extractor.FileExtractionStarted += (sender, args) => {
-                    statusDescription = String.Format("Extracting {0}", args.FileInfo.FileName);
+                    statusDescription = String.Format("Extracting file {0}", args.FileInfo.FileName);
                     WriteObject(statusDescription);
                 };
                 extractor.ExtractArchive(Directory);
             }
 
             WriteProgress(new ProgressRecord(0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
-            WriteObject(String.Format("Extraction finished"));
+            WriteObject("Extraction finished");
         }
     }
 }
