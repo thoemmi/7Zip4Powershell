aacf0f7d0481cf79574bb0676ffefea53bdf011d
Thomas Freudenberg <info@thomasfreudenberg.com>
2016-05-29 18:52:19 +0200

    added optional password to Get-7Zip

--- 7Zip4Powershell/Get7Zip.cs
+++ 7Zip4Powershell/Get7Zip.cs
@@ -2,30 +2,35 @@
 using System.Management.Automation;
 using SevenZip;
 
-namespace SevenZip4PowerShell
-{
+namespace SevenZip4PowerShell {
     [Cmdlet(VerbsCommon.Get, "7Zip")]
-    public class Get7Zip : PSCmdlet
-    {
+    public class Get7Zip : PSCmdlet {
         [Parameter(Position = 0, Mandatory = true, HelpMessage = "The full file name of the archive")]
         [ValidateNotNullOrEmpty]
         public string ArchiveFileName { get; set; }
 
-        protected override void BeginProcessing()
-        {
+        [Parameter]
+        public string Password { get; set; }
+
+        protected override void BeginProcessing() {
             SevenZipBase.SetLibraryPath(Utils.SevenZipLibraryPath);
         }
 
-        protected override void ProcessRecord()
-        {
-            var archiveFileName = new FileInfo(Path.Combine(this.SessionState.Path.CurrentFileSystemLocation.Path, this.ArchiveFileName)).FullName;
+        protected override void ProcessRecord() {
+            var archiveFileName =
+                new FileInfo(Path.Combine(this.SessionState.Path.CurrentFileSystemLocation.Path, this.ArchiveFileName)).FullName;
 
             WriteVerbose($"Getting archive data {archiveFileName}");
 
-            using (var extractor = new SevenZipExtractor(archiveFileName))
-            {
-                foreach(var file in extractor.ArchiveFileData)
-                {
+            SevenZipExtractor extractor;
+            if (!string.IsNullOrEmpty(Password)) {
+                extractor = new SevenZipExtractor(archiveFileName, Password);
+            } else {
+                extractor = new SevenZipExtractor(archiveFileName);
+            }
+
+            using (extractor) {
+                foreach (var file in extractor.ArchiveFileData) {
                     WriteObject(new PSObject(file));
                 }
             }
