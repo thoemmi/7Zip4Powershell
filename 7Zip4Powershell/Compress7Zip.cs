926b7ab6a402367c795fcc861f88a99af004461d
Kris Borowinski <kris.borowinski@outlook.com>
2020-03-31 02:20:02 +0200

    Replace SevenZipSharp.Net45 with Squid-Box.SevenZipSharp library

--- 7Zip4Powershell/Compress7Zip.cs
+++ 7Zip4Powershell/Compress7Zip.cs
@@ -30,7 +30,7 @@ namespace SevenZip4PowerShell {
         public string Path { get; set; }
 
         [Parameter(Position = 2, Mandatory = false, HelpMessage = "The filter to be applied if Path points to a directory")]
-        public string Filter { get; set; }
+        public string Filter { get; set; } = "*";
 
         private List<string> _directoryOrFilesFromPipeline;
 
@@ -217,13 +217,13 @@ namespace SevenZip4PowerShell {
                         if (HasPassword) {
                             compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, _cmdlet._password, _cmdlet.Filter, recursion);
                         } else {
-                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, _cmdlet.Filter, recursion);
+                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, null, _cmdlet.Filter, recursion);
                         }
                     } else {
                         if (HasPassword) {
-                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, recursion, _cmdlet._password);
+                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, _cmdlet._password, null, recursion);
                         } else {
-                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, recursion);
+                            compressor.CompressDirectory(directoryOrFiles[0], archiveFileName, null, null, recursion);
                         }
                     }
                 }
